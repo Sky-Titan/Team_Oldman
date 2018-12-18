822803b66afa578693a75f7fb2d8dd0cfaa60dac
Donghoon Song <thdehdgns@gmail.com>
2018-12-19 00:00:20 +0900

    Creating a thread and make it to work separately. So there are two threads now. One is the main thread and another is the one which create Fs and make them move

--- 동훈/villian.c
+++ 동훈/villian.c
@@ -13,23 +13,27 @@ struct villian
 	int orientation;
 };
 
-typedef struct villian vil;
+typedef struct villian vil; // "F" structure name
 
-clock_t s, f;
+clock_t s, f; // 시간잼수
 
-#define vil_num LINES - 13
+int t = 0, draw_flag = 0;
 
-// void ef_spawnpos(ef *f); // f 스폰위치, 컬러
-// void ef_speed(ef *f, int level); // f 스피드
-void gotoxy(int x, int y); // 좌표이동
-void textcolor(int color); // 컬러
+#define vil_num LINES - 13 // "F" 개수
+
+void move_villians(vil *vils);
+
+void draw_villians(vil *vils);
+
+void *p1_function(void *data);
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; // 쓰레드 초기화
 
 //game window LeftUp(12,1)~RightLow(LINE-2,COLS-2)
 
-
 void draw_villians(vil *vils)
 {
-	int i, flag=0;
+	int i, flag = 0;
 
 	for (i = 0; i < vil_num; i++)
 	{
@@ -37,14 +41,16 @@ void draw_villians(vil *vils)
 
 		flag = rand() % 2;
 
-		vils[i].y = rand() % (COLS-3) + 1;
+		vils[i].y = rand() % (COLS - 3) + 1;
 
 		if (flag == 0)
 			vils[i].orientation = 1; // 오른쪽으로 이동
-		else if(flag == 1)
+		else if (flag == 1)
 			vils[i].orientation = -1; // 왼쪽으로 이동
-
+		
+		// pthread_mutex_lock(&mutex);
 		mvprintw(vils[i].x, vils[i].y, "F");
+		// pthread_mutex_unlock(&mutex);
 	}
 }
 
@@ -52,22 +58,45 @@ void move_villians(vil *vils)
 {
 	int i;
 
-	for(i=0; i<vil_num; i++){
+	for (i = 0; i < vil_num; i++)
+	{
+		// pthread_mutex_lock(&mutex);
 		mvprintw(vils[i].x, vils[i].y, " ");
+		// pthread_mutex_unlock(&mutex);
 		refresh;
 		vils[i].y += vils[i].orientation;
+		// pthread_mutex_lock(&mutex);
 		mvprintw(vils[i].x, vils[i].y, "F");
+		// pthread_mutex_unlock(&mutex);
 		refresh;
-		if(vils[i].y==1 || vils[i].y==COLS-2)
+		if (vils[i].y == 1 || vils[i].y == COLS - 2)
 			vils[i].orientation *= (-1);
-		
+	}
+}
+
+void *p1_function(void *data)
+{
+	vil *vils;
+	vils = ((vil *)data); // F 구조체배열 받아오기
+
+	while (t < 50)
+	{
+		while (!draw_flag)
+		{	pthread_mutex_lock(&mutex);
+			draw_villians(vils);
+			pthread_mutex_unlock(&mutex);
+			draw_flag = 1;
+		}
+		pthread_mutex_lock(&mutex);
+		move_villians(vils);
+		pthread_mutex_unlock(&mutex);
+		sleep(1);
 	}
 }
 
 int main()
 {
-	int i, t = 0;
-	// clock_t s, f;
+	int i;
 
 	int level[4] = {1, 2, 3, 4};
 	int levels = 0;
@@ -76,13 +105,17 @@ int main()
 
 	int x0 = 12, nlines = LINES - 12, y0 = 0, ncols = COLS - 1; // new window의 꼭짓점들
 
-	int draw_flag = 0 ; //villain들이 draw된 횟수 (0이면 한번 그리고 1이면 그만 그려)
+	int draw_flag = 0; //villain들이 draw된 횟수 (0이면 한번 그리고 1이면 그만 그려)
 
 	pthread_t p_thread[1];
 
-	// int vil_num = LINES-12; // F 개수
+	int thr_id[1];
 
-	// vil vils[vil_num]; // 악당 구조체 배열 50개
+	int thr_flag[1] = {
+		0,
+	}; // thread 실행횟수
+
+	int status;
 
 	initscr();   // reset curses
 	clear();	 // window clear
@@ -94,7 +127,7 @@ int main()
 	// WINDOW *win = newwin(LINES, COLS, x0, y0);
 
 	// 12, LINES
-
+	//--------------------------------------------------------------------
 	// window frame
 	for (i = 1; i < COLS - 1; i++)
 	{
@@ -121,51 +154,64 @@ int main()
 	move(4, 5);
 	addstr("This game is to get A grade while avoiding F grades");
 	refresh();
-
+	//--------------------------------------------------------------------------
 	vil *vils = (vil *)malloc(sizeof(vil) * vil_num);
 
 	//time calculate and step appear
 	s = clock();
 
-	// villians appear
-	// while(1){
+	thr_id[0] = pthread_create(&p_thread[0], NULL, p1_function, (void *)&vils);
 
-	// }
-	// draw_villians(vils);
-	while (1)
+	if(thr_id[0] < 0)
 	{
+		perror("thread create error : ");
+		exit(0);
+	}
+
+	while (t < 50)
+	{
+		pthread_mutex_lock(&mutex);
 		move(7, 5);
-		if (t < 50)
-		{
-			printw("| Stage %d | Time %3d |", level[0], t);
-			while(!draw_flag)
-			{
-				draw_villians(vils);
-				draw_flag=1;
-			}
-			move_villians(vils);
-			sleep(1);
-		}
-		else if (t < 60)
-			printw("| Stage %d | Time %3d | Boss Detected |", level[1], t);
-		else if (t < 160)
-			printw("| Stage %d | Time %3d |                    ", 2, t);
-		else if (t < 180)
-			printw("| Stage %d | Time %3d | Boss Detected |", level[2], t);
-		else if (t < 330)
-			printw("| Stage %d | Time %3d |                    ", 3, t);
-		else if (t < 360)
-			printw("| Stage %d | Time %3d | Boss Detected |", level[3], t);
-		else if (t < 560)
-			printw("| Stage %d | Time %3d |                    ", 4, t);
-		else if (t < 600)
-			printw("| Stage %d | Time %3d | Boss Detected |", level[4], t);
-		else
-			break;
+		printw("| Stage %d | Time %3d |", level[0], t);
 		refresh();
+		pthread_mutex_unlock(&mutex);
 		f = clock();
 		t = (int)(f - s) / 1000000;
 	}
+	pthread_join(p_thread[0], (void **)&status);
+
+
+	// while (t >= 50 && t < 60)
+	// {
+	// 	pthread_mutex_lock(&mutex);
+	// 	move(7, 5);
+	// 	printw("| Stage %d | Time %3d |", level[0], t);
+	// 	refresh();
+	// 	pthread_mutex_unlock(&mutex);
+	// 	f = clock();
+	// 	t = (int)(f - s) / 1000000;
+	// }
+	// pthread_join(p_thread[0], (void **)&status);
+
+	// else if (t < 60)
+	// 	printw("| Stage %d | Time %3d | Boss Detected |", level[1], t);
+	// else if (t < 160)
+	// 	printw("| Stage %d | Time %3d |                    ", 2, t);
+	// else if (t < 180)
+	// 	printw("| Stage %d | Time %3d | Boss Detected |", level[2], t);
+	// else if (t < 330)
+	// 	printw("| Stage %d | Time %3d |                    ", 3, t);
+	// else if (t < 360)
+	// 	printw("| Stage %d | Time %3d | Boss Detected |", level[3], t);
+	// else if (t < 560)
+	// 	printw("| Stage %d | Time %3d |                    ", 4, t);
+	// else if (t < 600)
+	// 	printw("| Stage %d | Time %3d | Boss Detected |", level[4], t);
+	// else break;
+	// refresh();
+	// f = clock();
+	// t = (int)(f - s) / 1000000;
+	// }
 	//result print
 	move(9, 5);
 	if (t < 60)
